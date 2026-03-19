6c263a6670b1a30b0cf55ad945253243c96684f1
roelselten1 <71645324+roelselten1@users.noreply.github.com>
2026-03-19 16:15:23 +0100

    Update Booqi theme navigation fallbacks

--- booqi-classic/footer.php
+++ booqi-classic/footer.php
@@ -5,58 +5,6 @@
  * @package BooqiClassic
  */
 ?>
-<?php
-$booqi_footer_company_links = array(
-	array(
-		'label' => __( 'Features', 'booqi-classic' ),
-		'url'   => home_url( '/features' ),
-	),
-	array(
-		'label' => __( 'Pricing', 'booqi-classic' ),
-		'url'   => home_url( '/#pricing' ),
-	),
-	array(
-		'label' => __( 'FAQ', 'booqi-classic' ),
-		'url'   => home_url( '/#faq' ),
-	),
-	array(
-		'label' => __( 'About Us', 'booqi-classic' ),
-		'url'   => home_url( '/about-us' ),
-	),
-	array(
-		'label' => __( 'Blog', 'booqi-classic' ),
-		'url'   => home_url( '/blog' ),
-	),
-);
-
-$booqi_footer_useful_links = array(
-	array(
-		'label'  => __( 'Support', 'booqi-classic' ),
-		'url'    => 'https://support.booqi.me/',
-		'target' => '_blank',
-	),
-	array(
-		'label'  => __( 'Helpcentrum', 'booqi-classic' ),
-		'url'    => 'https://support.booqi.me/',
-		'target' => '_blank',
-	),
-	array(
-		'label'  => __( 'API Documentatie', 'booqi-classic' ),
-		'url'    => 'https://booqi.docs.apiary.io/',
-		'target' => '_blank',
-	),
-	array(
-		'label'  => __( 'Roadmap', 'booqi-classic' ),
-		'url'    => 'https://trello.com/',
-		'target' => '_blank',
-	),
-	array(
-		'label'  => __( 'Status page', 'booqi-classic' ),
-		'url'    => 'https://status.booqi.app/',
-		'target' => '_blank',
-	),
-);
-?>
 	</main>
 	<footer class="site-footer">
 		<div class="site-container">
@@ -70,51 +18,59 @@ $booqi_footer_useful_links = array(
 					</div>
 
 					<div class="site-footer__column">
-						<p class="site-footer__heading">Booqi.me</p>
-						<ul class="site-footer__menu">
-							<?php foreach ( $booqi_footer_company_links as $booqi_footer_company_link ) : ?>
-								<li>
-									<a href="<?php echo esc_url( $booqi_footer_company_link['url'] ); ?>">
-										<?php echo esc_html( $booqi_footer_company_link['label'] ); ?>
-									</a>
-								</li>
-							<?php endforeach; ?>
-						</ul>
+						<p class="site-footer__heading"><?php esc_html_e( 'Booqi.me', 'booqi-classic' ); ?></p>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'footer_company',
+								'container'      => false,
+								'menu_class'     => 'site-footer__menu',
+								'fallback_cb'    => 'booqi_classic_footer_company_menu_fallback',
+							)
+						);
+						?>
 					</div>
 
 					<div class="site-footer__column">
 						<p class="site-footer__heading"><?php esc_html_e( 'Useful links', 'booqi-classic' ); ?></p>
-						<ul class="site-footer__menu">
-							<?php foreach ( $booqi_footer_useful_links as $booqi_footer_useful_link ) : ?>
-								<li>
-									<a href="<?php echo esc_url( $booqi_footer_useful_link['url'] ); ?>" target="<?php echo esc_attr( $booqi_footer_useful_link['target'] ); ?>" rel="noreferrer noopener">
-										<?php echo esc_html( $booqi_footer_useful_link['label'] ); ?>
-									</a>
-								</li>
-							<?php endforeach; ?>
-						</ul>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'footer_useful',
+								'container'      => false,
+								'menu_class'     => 'site-footer__menu',
+								'fallback_cb'    => 'booqi_classic_footer_useful_menu_fallback',
+							)
+						);
+						?>
 					</div>
 
 					<div class="site-footer__column site-footer__column--contact">
 						<p class="site-footer__heading"><?php esc_html_e( 'Contact', 'booqi-classic' ); ?></p>
 						<address class="site-footer__address">
-							<span>Velmolenweg 54a,</span>
-							<span>5404LD</span>
-							<strong>Uden NL</strong>
+							<span><?php esc_html_e( 'Velmolenweg 54a,', 'booqi-classic' ); ?></span>
+							<span><?php esc_html_e( '5404LD', 'booqi-classic' ); ?></span>
+							<strong><?php esc_html_e( 'Uden NL', 'booqi-classic' ); ?></strong>
 						</address>
 						<ul class="site-footer__menu site-footer__menu--contact">
-							<li><a href="tel:+31850044219">+31 85 004 42 19</a></li>
-							<li><a href="mailto:info@booqi.me">info@booqi.me</a></li>
+							<li><a href="<?php echo esc_url( 'tel:+31850044219' ); ?>">+31 85 004 42 19</a></li>
+							<li><a href="<?php echo esc_url( 'mailto:info@booqi.me' ); ?>">info@booqi.me</a></li>
 						</ul>
 					</div>
 				</div>
 
 				<div class="site-footer__bottom">
 					<p><?php echo esc_html( sprintf( __( '©%s Copyright | All right reserved', 'booqi-classic' ), gmdate( 'Y' ) ) ); ?></p>
-					<div class="site-footer__legal">
-						<a href="<?php echo esc_url( home_url( '/privacy-cookie-policy' ) ); ?>"><?php esc_html_e( 'Privacy & Cookie Policy', 'booqi-classic' ); ?></a>
-						<a href="<?php echo esc_url( home_url( '/terms-and-conditions' ) ); ?>"><?php esc_html_e( 'General Terms & Conditions', 'booqi-classic' ); ?></a>
-					</div>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'footer_legal',
+							'container'      => false,
+							'menu_class'     => 'site-footer__legal',
+							'fallback_cb'    => 'booqi_classic_footer_legal_menu_fallback',
+						)
+					);
+					?>
 				</div>
 			</div>
 		</div>
